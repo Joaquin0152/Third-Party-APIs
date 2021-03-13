a2273d28f37260bb83324d9bf74f04ef81185cca
Joaquin Maravillo <joaquin297545@gmail.com>
2021-03-13 16:48:34 -0600

    finished adding a function on the save button listed on each row

--- java.js
+++ java.js
@@ -1,43 +1,30 @@
 // time DOM elements
 var timeDisplayEl = $('#time-display');
 var dateInputEl = $('#datepicker');
-var clear = $('input["text"]');
+var clearButton = document.getElementById("clearBtn");
+var saveButton = document.getElementById("saveBtn");
 
 // handle displaying the time
 function displayTime() {
   var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
-}
+};
+
+
+//save button
+$(".saveBtn").on("click", function(){
+  userInput = $(this).siblings(".form-control").val().trim();
+  console.log(userInput);
+  hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+  console.log(hourSpan);
+  localStorage.setItem(hourSpan, JSON.stringify(userInput));
+
+})
+
+  $("#clear").on("click", function(){
+    localStorage.clear();
+    initPage()
+  }) 
+
 setInterval(displayTime, 1000);
 //boom it works!!!!! 
-
-function resetButton(event){
-  event.preventDefault();
-  $('input["text"]').val('');
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // $(document).ready(function(){
-  //   console.log("ready!");
-  //   moment().day("Sunday");
-  //   console.log(momnet().hour());
-  //   $(#"currendDay").text(moment().format('dddd,mmmm Do'));
-  //   $('textarea').removeClass('past').addClass('future');
-  //   if currentTime = time dislplay correct textarea aka color = red
-  //   before <=currentTime gray
-  //   future = green
-
-  // });
