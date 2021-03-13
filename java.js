96d2aee13e1230b22c4df5e44347e469568182c9
Joaquin Maravillo <joaquin297545@gmail.com>
2021-03-13 15:51:57 -0600

    added a function to reset all the content on the form

--- java.js
+++ java.js
@@ -1,6 +1,7 @@
 // time DOM elements
 var timeDisplayEl = $('#time-display');
 var dateInputEl = $('#datepicker');
+var clear = $('input["text"]');
 
 // handle displaying the time
 function displayTime() {
@@ -10,4 +11,33 @@ function displayTime() {
 setInterval(displayTime, 1000);
 //boom it works!!!!! 
 
-  
+function resetButton(event){
+  event.preventDefault();
+  $('input["text"]').val('');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // $(document).ready(function(){
+  //   console.log("ready!");
+  //   moment().day("Sunday");
+  //   console.log(momnet().hour());
+  //   $(#"currendDay").text(moment().format('dddd,mmmm Do'));
+  //   $('textarea').removeClass('past').addClass('future');
+  //   if currentTime = time dislplay correct textarea aka color = red
+  //   before <=currentTime gray
+  //   future = green
+
+  // });
