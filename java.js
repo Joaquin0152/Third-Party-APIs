a4ec7a82b2a3982862fe96457d7a6eba3cf0e608
Joaquin Maravillo <joaquin297545@gmail.com>
2021-03-13 17:27:56 -0600

    added function in order for different time variables to keep the data in local storage

--- java.js
+++ java.js
@@ -26,5 +26,38 @@ $(".saveBtn").on("click", function(){
     initPage()
   }) 
 
+  function initPage(){
+    var init9 = JSON.parse(localStorage.getItem("06:30 am"));
+  sixAm.val(init6);
+
+  var init10 = JSON.parse(localStorage.getItem("07:30 am"))
+  sevenAm.val(init7);
+  
+  var init11 = JSON.parse(localStorage.getItem("08:30 am"))
+  eightAm.val(init8);
+  
+  var init12 = JSON.parse(localStorage.getItem("09:30 am"))
+  nineAm.val(init9);
+  
+  var init1 = JSON.parse(localStorage.getItem("10:30 am"))
+  tenAm.val(init10);
+  
+  var init2 = JSON.parse(localStorage.getItem("11:30 pm"))
+  elevenAm.val(ini11);
+  
+  var init3 = JSON.parse(localStorage.getItem("12:30pm"))
+  twelvePm.val(init12);
+ 
+  var init4 = JSON.parse(localStorage.getItem("01:30 pm"))
+  onePm.val(init1);
+  
+  var init5 = JSON.parse(localStorage.getItem("02:30pm"))
+  twoPm.val(init2);
+  
+  var init6 = JSON.parse(localStorage.getItem("03:30 pm"))
+  threePm.val(init3);
+
+} 
+
 setInterval(displayTime, 1000);
 //boom it works!!!!! 
